c5fc382f387eec7fc929e888bc8636836f127865
leo1q <laraya1818@gmail.com>
2021-12-10 03:50:05 -0300

    Added ascii launcher

--- src/SampFresh.py
+++ src/SampFresh.py
@@ -2,8 +2,28 @@ from winreg import OpenKey, QueryValueEx, SetValueEx, HKEY_CURRENT_USER, KEY_REA
 from subprocess import call, check_output
 from pypresence import Presence
 from time import sleep
+from colorama import init, Fore
+
+AUTHORS = "le01q and manucrabral"
+
+LOGO = f'''
+{Fore.GREEN}
+.d8888b.         d8888 888b     d888  8888888b.      8888888888                       888      
+d88P  Y88b      d88888 8888b   d8888 888   Y88b      888                              888      
+Y88b.          d88P888 88888b.d88888 888    888      888                              888      
+ "Y888b.      d88P 888 888Y88888P888 888   d88P      8888888 888d888 .d88b.  .d8888b  88888b.  
+    "Y88b.   d88P  888 888 Y888P 888 8888888P"       888     888P"  d8P  Y8b 88K      888 "88b 
+      "888  d88P   888 888  Y8P  888 888             888     888    88888888 "Y8888b. 888  888 
+Y88b  d88P d8888888888 888   "   888 888             888     888    Y8b.          X88 888  888 
+ "Y8888P" d88P     888 888       888 888             888     888     "Y8888   88888P' 888  888
+{Fore.RESET}
+
+Authors: le01q & manucabral
+'''
 
 class SampFresh:
+    print(LOGO)
+
     def __init__(self, client_id):
         self.samp_path = None
         self.player_name = None
@@ -34,7 +54,7 @@ class SampFresh:
         return self.player_name
 
     def ChangePlayerName(self):
-        new_name = str(input("New name: "))
+        new_name = str(input("[!] New name: "))
         self.SetKetValue(HKEY_CURRENT_USER, r'SOFTWARE\SAMP', 'PlayerName', new_name)
         self.player_name = new_name
         
@@ -58,10 +78,10 @@ class SampFresh:
         
         self.GetPlayerName()
         self.GetSampPath()
-        
-        print('Current nickname: ', self.player_name)
-        option = str(input('Wanna change your nickname? (Yes or No)'))
-        if 'y' in option.lower():
+
+        print('[>] Current nickname: ', self.player_name)
+        option = str(input('[?] Wanna change your nickname? (Yes or No): '))
+        if 'Yes' in option.lower():
             self.ChangePlayerName()
 
         self.SetAddress()
@@ -71,12 +91,12 @@ class SampFresh:
 
         while self.gta_running:
             self.gta_running = self.IsGtaRunning()
-            self.presence.update(details="Playing in" + self.address)
+            self.presence.update(details="Playing in " + self.address)
             sleep(15)
 
         self.presence.close()
 
 
 if __name__ == '__main__':
-    client = SampFresh(':D')
+    client = SampFresh('918675263980724264')
     client.run()
