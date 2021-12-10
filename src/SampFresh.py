e14ea335f078d7484e4e1f9aabcacb6af5caa487
Manu <mcabral@frba.utn.edu.ar>
2021-12-10 09:48:44 -0300

    added some exceptions

--- src/SampFresh.py
+++ src/SampFresh.py
@@ -3,26 +3,9 @@ from subprocess import call, check_output
 from pypresence import Presence
 from time import sleep
 from colorama import init, Fore
-
-AUTHORS = "le01q and manucrabral"
-
-LOGO = f'''
-{Fore.GREEN}
-.d8888b.         d8888 888b     d888  8888888b.      8888888888                       888      
-d88P  Y88b      d88888 8888b   d8888 888   Y88b      888                              888      
-Y88b.          d88P888 88888b.d88888 888    888      888                              888      
- "Y888b.      d88P 888 888Y88888P888 888   d88P      8888888 888d888 .d88b.  .d8888b  88888b.  
-    "Y88b.   d88P  888 888 Y888P 888 8888888P"       888     888P"  d8P  Y8b 88K      888 "88b 
-      "888  d88P   888 888  Y8P  888 888             888     888    88888888 "Y8888b. 888  888 
-Y88b  d88P d8888888888 888   "   888 888             888     888    Y8b.          X88 888  888 
- "Y8888P" d88P     888 888       888 888             888     888     "Y8888   88888P' 888  888
-{Fore.RESET}
-
-Authors: le01q & manucabral
-'''
+from constants import *
 
 class SampFresh:
-    print(LOGO)
 
     def __init__(self, client_id):
         self.samp_path = None
@@ -53,50 +36,83 @@ class SampFresh:
         self.player_name = self.GetKeyValue(HKEY_CURRENT_USER, r'SOFTWARE\SAMP', 'PlayerName')
         return self.player_name
 
-    def ChangePlayerName(self):
-        new_name = str(input("[!] New name: "))
-        self.SetKetValue(HKEY_CURRENT_USER, r'SOFTWARE\SAMP', 'PlayerName', new_name)
+    def ChangePlayerName(self, new_name):
+        try:
+            self.SetKetValue(HKEY_CURRENT_USER, r'SOFTWARE\SAMP', 'PlayerName', new_name)
+        except Exception:
+            raise
         self.player_name = new_name
         
-    def SetAddress(self):
-        ip = str(input("IP: "))
-        self.host, self.port = ip.split(':')[0], ip.split(':')[1]
-        self.address = ip
-
-    def StartSamp(self):
-        args = [self.samp_path, self.address]
-        print(self.address)
-        call(args)
+    def IpAddressEvent(self):
+        host, port = self.InputIp()
+        self.address = f'{host}:{port}'
     
-    def Init(self):
-        return not self.IsProcessRunning('gta_sa.exe') and not self.IsProcessRunning('samp.exe')
+    def InputIp(self):
+        while True:
+            try:
+                ip = str(input("[>] Server IP: "))
+                host, port = ip.split(':')[0], ip.split(':')[1]
+                return host, port
+            except IndexError:
+                self.message(Fore.RED, "Invalid IP, example: 192.56.67.8:7777")
+    
+    def StartProcess(self, args=[]):
+        call(args)
+        self.gta_running = True
+
+    def message(self, color, msg):
+        print(color + msg + Fore.RESET)
+
+    def Initialize(self):
+        init()
+        if not self.GetSampPath():
+            raise Exception("Can't get SA-MP path")
+        if not self.GetPlayerName():
+            raise Exception("Can't get player name")
+        return
+        #return not self.IsProcessRunning('gta_sa.exe') and not self.IsProcessRunning('samp.exe')
+    
+    def DisplayBanner(self):
+        self.message(Fore.GREEN, LOGO)
+        self.message(Fore.YELLOW, "[>] Current nickname: " + self.player_name)
+    
+    def InputPlayerName(self):
+        while True:
+            name = str(input('[>] New name: '))
+            if 4 <= len(name) < 24:
+                return name
+        
+    def PlayerNameEvent(self):
+        option = str(input('[?] Wanna change your nickname? (Yes or No): '))
+        if 'y' in option.lower():
+            self.ChangePlayerName(self.InputPlayerName())
 
     def run(self):
-        if not self.Init():
-            print("Can't execute")
+        try:
+            try:
+                self.Initialize()
+            except Exception as e:
+                return self.message(Fore.RED, e)
+
+            self.DisplayBanner()
+            self.PlayerNameEvent()
+            self.IpAddressEvent()
+
+            self.StartProcess([self.samp_path, self.address])
+
+            self.presence.connect()
+
+            while self.gta_running:
+                self.gta_running = self.IsProcessRunning('gta_sa.exe')
+                self.presence.update(details="Playing in " + self.address)
+                sleep(15)
+
+            self.presence.close()
+
+        except KeyboardInterrupt:
             return
-        
-        self.GetPlayerName()
-        self.GetSampPath()
-
-        print('[>] Current nickname: ', self.player_name)
-        option = str(input('[?] Wanna change your nickname? (Yes or No): '))
-        if 'Yes' in option.lower():
-            self.ChangePlayerName()
-
-        self.SetAddress()
-        self.StartSamp()
-
-        self.presence.connect()
-
-        while self.gta_running:
-            self.gta_running = self.IsGtaRunning()
-            self.presence.update(details="Playing in " + self.address)
-            sleep(15)
-
-        self.presence.close()
 
 
 if __name__ == '__main__':
-    client = SampFresh('918675263980724264')
+    client = SampFresh(':D')
     client.run()
