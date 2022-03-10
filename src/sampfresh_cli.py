ad55c61e8eb9c0df16c92262ba6d8cad15aaf659
manucabral <mcabral@frba.utn.edu.ar>
2022-03-10 20:47:07 -0300

    updated cli by leo

--- src/sampfresh_cli.py
+++ src/sampfresh_cli.py
@@ -2,16 +2,16 @@ from winreg import OpenKey, QueryValueEx, SetValueEx, HKEY_CURRENT_USER, KEY_REA
 from socket import gethostbyname, socket, AF_INET, SOCK_DGRAM
 from subprocess import call, check_output
 from pypresence import Presence
-from time import sleep
 from colorama import init, Fore
 from constants import *
+import time
+import os
 
 class SampFresh:
 
     def __init__(self, client_id):
         self.samp_path = self.player_name = self.host = self.address = None
-        self.port = 7777 # default port
-        self.presence = Presence(client_id)
+        self.port = 7777
         self.sock = socket(AF_INET, SOCK_DGRAM)
         self.query_header = b'SAMP'
         self.gta_running = self.samp_running = False
@@ -19,7 +19,7 @@ class SampFresh:
     def IsProcessRunning(self, process_name):
         return True if process_name in check_output("tasklist", universal_newlines=True) else False
 
-    def SetKetValue(self, Key, Subkey, name, value):
+    def SetKeyValue(self, Key, Subkey, name, value):
         result = OpenKey(Key, Subkey, 0, KEY_ALL_ACCESS)
         return SetValueEx(result, name, 0, REG_SZ, value)
     
@@ -37,7 +37,7 @@ class SampFresh:
 
     def ChangePlayerName(self, new_name):
         try:
-            self.SetKetValue(HKEY_CURRENT_USER, r'SOFTWARE\SAMP', 'PlayerName', new_name)
+            self.SetKeyValue(HKEY_CURRENT_USER, r'SOFTWARE\SAMP', 'PlayerName', new_name)
         except Exception:
             raise
         self.player_name = new_name
@@ -49,11 +49,11 @@ class SampFresh:
     def InputIp(self):
         while True:
             try:
-                ip = str(input("[SA:MP Fresh] Write the server IP address below: "))
+                ip = str(input("[SA:MP Fresh CLI] Specify the server IP address here: "))
                 host, port = ip.split(':')[0], ip.split(':')[1]
                 return host, port
             except IndexError:
-                self.message(Fore.RED, "[SA:MP Fresh | ERROR] Please specify a valid IP address.")
+                self.message(Fore.RED, "[SA:MP Fresh CLI | ERROR] Please specify a valid IP address.")
     
     def StartProcess(self, args=[]):
         call(args)
@@ -63,7 +63,7 @@ class SampFresh:
         print(color + msg + Fore.RESET)
 
     def Initialize(self):
-        init()
+        init(autoreset=True)
         if not self.GetSampPath():
             raise Exception("Can't get SA-MP path")
         if not self.GetPlayerName():
@@ -71,19 +71,36 @@ class SampFresh:
         return
     
     def DisplayBanner(self):
-        self.message(Fore.BLUE, PROJECT_LOGO)
-        self.message(Fore.YELLOW, "[SA:MP Fresh] Current nickname: " + self.player_name)
+        self.message(Fore.RESET, MAIN)
+        self.message(Fore.GREEN, "[SA:MP Fresh CLI] This is your current nickname in SA:MP: <" + self.player_name + ">")
     
     def InputPlayerName(self):
         while True:
-            name = str(input('[SA:MP Fresh] New nickname: '))
+            name = str(input("[SA:MP Fresh CLI] Write your new nickname that you will use: "))
             if 4 <= len(name) < 24:
                 return name
         
     def PlayerNameEvent(self):
-        option = str(input('[SA:MP Fresh] Wanna change your nickname game? (Y/N): '))
+        option = str(input("[SA:MP Fresh CLI] You want change your current nickname? (Y/N): "))
         if 'y' in option.lower():
             self.ChangePlayerName(self.InputPlayerName())
+
+    def CommandLineRichPresence(self):
+        self.client_id = "943925992550981652"
+        RPC = Presence(self.client_id)
+        RPC.connect()
+        
+        while True:
+            nickname = self.player_name
+            server = self.address
+
+            RPC.update(
+                state="Server IP: " + server, 
+                details="Nickname: " + nickname, 
+                large_image="sampfresh_cli", 
+                large_text="Playing from CLI",
+            )
+            time.sleep(15)
 
     def run(self):
         try:
@@ -99,8 +116,8 @@ class SampFresh:
             self.StartProcess([self.samp_path, self.address])
 
             while self.gta_running:
+                self.CommandLineRichPresence()
                 self.gta_running = self.IsProcessRunning('gta_sa.exe')
-                self.message(Fore.GREEN, "[SA:MP Fresh] You have been connected to: " + self.address)
 
         except KeyboardInterrupt:
             return
